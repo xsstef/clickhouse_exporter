0147c8a0c350e0ac697b02c7a115a496fae898cf
wangqingping <eliotwang@tencent.com>
2021-04-08 20:19:58 +0800

    fix parse non integer cpu frequency error.

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -203,7 +203,7 @@ func (e *Exporter) handleResponse(uri string) ([]byte, error) {
 
 type lineResult struct {
 	key   string
-	value int
+	value float64
 }
 
 func (e *Exporter) parseKeyValueResponse(uri string) ([]lineResult, error) {
@@ -225,7 +225,7 @@ func (e *Exporter) parseKeyValueResponse(uri string) ([]lineResult, error) {
 			return nil, fmt.Errorf("parseKeyValueResponse: unexpected %d line: %s", i, line)
 		}
 		k := strings.TrimSpace(parts[0])
-		v, err := strconv.Atoi(strings.TrimSpace(parts[1]))
+		v, err := strconv.ParseFloat(strings.TrimSpace(parts[1]), 64)
 		if err != nil {
 			return nil, err
 		}
